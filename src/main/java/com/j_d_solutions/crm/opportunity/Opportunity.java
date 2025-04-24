94bc041369dd82c68a7f4e5285284e60941e849f
DDR144 <luis.fernando.didier.hr@gmail.com>
2025-04-23 22:54:08 -0500

    Feat: Agregando atributos a Opportunity y Task

--- src/main/java/com/j_d_solutions/crm/opportunity/Opportunity.java
+++ src/main/java/com/j_d_solutions/crm/opportunity/Opportunity.java
@@ -1,4 +1,22 @@
 package com.j_d_solutions.crm.opportunity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "opportunities")
 public class Opportunity {
+    private Integer idopportunity;
+    private String title;
+    @ManyToOne
+    @JoinColumn(name = "idclient")
+    private String status;
+    private Double value;
+    private Date due_date;
 }
--- src/main/java/com/j_d_solutions/crm/task/Task.java
+++ src/main/java/com/j_d_solutions/crm/task/Task.java
@@ -1,4 +1,23 @@
 package com.j_d_solutions.crm.task;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "tasks")
 public class Task {
+    private Integer idtask;
+    private String title;
+    @ManyToOne
+    @JoinColumn(name = "idclient")
+    private Integer idclient;
+    private String type;
+    private Date due_date;
+    private String notes;
 }
