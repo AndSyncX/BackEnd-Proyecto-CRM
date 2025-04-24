4956949467a0018f7b8a9c21174975406a3163ae
AndSyncX <fernandezp.joe@gmail.com>
2025-04-23 23:52:24 -0500

    Feat: ClientService

--- src/main/java/com/j_d_solutions/crm/client/ClientRepository.java
+++ src/main/java/com/j_d_solutions/crm/client/ClientRepository.java
@@ -1,6 +1,26 @@
 package com.j_d_solutions.crm.client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ClientRepository extends JpaRepository<Client , Integer> {
+    @Query(value = """
+            UPDATE clients SET
+            name = :name,
+            company = :company,
+            email = :email,
+            phone = :phone,
+            notes = :notes,
+            iduser = :iduser
+            WHERE idclient = :idclient
+            """, nativeQuery = true)
+    void updateParcialClient(
+            @Param("name") String name,
+            @Param("company") String company,
+            @Param("phone") String phone,
+            @Param("notes") String notes,
+            @Param("iduser") Integer iduser,
+            @Param("idclient") Integer idclient
+    );
 }
--- src/main/java/com/j_d_solutions/crm/client/ClientService.java
+++ src/main/java/com/j_d_solutions/crm/client/ClientService.java
@@ -16,5 +16,44 @@ public class ClientService {
         return clientRepository.findAll();
     }
 
+    public Client getClientById(int id) {
+        return clientRepository.findById(id).orElse(null);
+    }
 
+    public ClientDto getClientDtoById(int id) {
+        Client client = getClientById(id);
+        ClientDto clientDto = new ClientDto();
+        clientDto.setIdclient(client.getIdclient());
+        clientDto.setName(clientDto.getName());
+        clientDto.setCompany(client.getCompany());
+        clientDto.setEmail(client.getEmail());
+        clientDto.setPhone(client.getPhone());
+        clientDto.setNotes(client.getNotes());
+        clientDto.setIduser(client.getUser().getId());
+        return clientDto;
+    }
+
+    public void saveClient(ClientDto clientDto) {
+        if (clientDto.getIdclient() != null ){
+            //Es una actualización
+            clientRepository.updateParcialClient(
+                    clientDto.getName(),
+                    clientDto.getCompany(),
+                    clientDto.getPhone(),
+                    clientDto.getNotes(),
+                    clientDto.getIduser(),
+                    clientDto.getIdclient()
+            );
+        } else {
+            //Es un registro nuevo
+            Client client = new Client();
+            client.setName(clientDto.getName());
+            client.setCompany(clientDto.getCompany());
+            client.setEmail(clientDto.getEmail());
+            client.setPhone(clientDto.getPhone());
+            client.setNotes(clientDto.getNotes());
+            client.setUser(userRepository.findById(clientDto.getIduser()).orElse(null));
+            clientRepository.save(client);
+        }
+    }
 }
